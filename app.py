c72ef1166fd8fb04528b04e0a3b02ca76b253622
Tuomas Marenk <tuomas.marenk@gmail.com>
2022-04-01 11:44:05 +0300

    Better user check

--- app.py
+++ app.py
@@ -32,18 +32,16 @@ def movie(id):
 
 @app.route("/new_review/<int:id>")
 def new_review(id):
-    try:
-        if session["username"]:
-            sql = "SELECT name FROM Movies WHERE id=:id"
-            result = db.session.execute(sql, {"id":id})
-            movie = result.fetchone()
-            username = session['username']
-            sql = "SELECT id FROM users WHERE username=:username"
-            result = db.session.execute(sql, {"username":username})
-            user_id = result.fetchone()[0]
-            return render_template("new_review.html", id=id, movie=movie, user_id=user_id)
-    except:
-        return render_template("new_review.html")
+    if is_user():
+        sql = "SELECT name FROM Movies WHERE id=:id"
+        result = db.session.execute(sql, {"id":id})
+        movie = result.fetchone()
+        username = session['username']
+        sql = "SELECT id FROM users WHERE username=:username"
+        result = db.session.execute(sql, {"username":username})
+        user_id = result.fetchone()[0]
+        return render_template("new_review.html", id=id, movie=movie, user_id=user_id)
+    return render_template("new_review.html")
 
 @app.route("/leave_review", methods=["POST"])
 def leave_review():
@@ -61,104 +59,120 @@ def leave_review():
 
 @app.route("/login",methods=["GET", "POST"])
 def login():
-    try:
-        if session["username"]:
-            return redirect("/")
-    except:
-        if request.method == 'GET':
+    if is_user():
+        return redirect("/")
+    if request.method == 'GET':
+        return render_template("login.html")
+    if request.method == 'POST':
+        username = request.form["username"]
+        password = request.form["password"]
+        sql = "SELECT id, password FROM users WHERE LOWER(username)=LOWER(:username)"
+        result = db.session.execute(sql, {"username":username})
+        user = result.fetchone()
+        if not user:
+            flash("Käyttäjänimi väärin")
             return render_template("login.html")
-        if request.method == 'POST':
-            username = request.form["username"]
-            password = request.form["password"]
-            sql = "SELECT id, password FROM users WHERE LOWER(username)=LOWER(:username)"
-            result = db.session.execute(sql, {"username":username})
-            user = result.fetchone()
-            if not user:
-                flash("Käyttäjänimi väärin")
-                return render_template("login.html")
+        else:
+            hash_value = user.password
+            if check_password_hash(hash_value, password):
+                flash("Kirjautuminen onnistui")
+                session["username"] = username
+                return redirect("/")
             else:
-                hash_value = user.password
-                if check_password_hash(hash_value, password):
-                    flash("Kirjautuminen onnistui")
-                    session["username"] = username
-                    return redirect("/")
-                else:
-                    flash("Väärä salasana")
-                    return render_template("login.html")
+                flash("Väärä salasana")
+                return render_template("login.html")
 
 @app.route("/registeration",methods=["GET", "POST"])
 def register():
-    try:
-        if session["username"]:
-            return redirect("/")
-    except:
-        if request.method == "GET":
-            return render_template("register.html")
-        if request.method == 'POST':
-            username = request.form["username"]
-            password1 = request.form["password1"]
-            password2 = request.form["password2"]
-            sql = "SELECT username FROM users WHERE LOWER(username)=LOWER(:username)"
-            result = db.session.execute(sql, {"username":username})
-            exists = result.fetchone()
-            if exists != None:
-                error = 'Käyttäjänimi on jo olemassa'
-                return render_template("register.html", error=error)
-            length = len(username)
-            if length < 3:
-                error = 'Käyttäjänimessa tulee olla vähintään 3 merkkiä'
-                return render_template("register.html", error=error)
-            if length > 16:
-                error = "Käyttäjänimessä saa olla enintään 16 merkkiä"
-                return render_template("register.html", error=error)
-            if " " in username:
-                error = "Käyttäjänimessä ei saa olla välilyöntejä"
-                return render_template("register.html", error=error)
-            noletters = True
-            for i in username:
+    if is_user():     
+        return redirect("/")
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == 'POST':
+        username = request.form["username"]
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+        sql = "SELECT username FROM users WHERE LOWER(username)=LOWER(:username)"
+        result = db.session.execute(sql, {"username":username})
+        exists = result.fetchone()
+        if exists != None:
+            error = 'Käyttäjänimi on jo olemassa'
+            return render_template("register.html", error=error)
+        length = len(username)
+        if length < 3:
+            error = 'Käyttäjänimessa tulee olla vähintään 3 merkkiä'
+            return render_template("register.html", error=error)
+        if length > 16:
+            error = "Käyttäjänimessä saa olla enintään 16 merkkiä"
+            return render_template("register.html", error=error)
+        if " " in username:
+            error = "Käyttäjänimessä ei saa olla välilyöntejä"
+            return render_template("register.html", error=error)
+        noletters = True
+        for i in username:
+            if i.isalpha():
+                noletters = False
+                break
+        if noletters:
+            error = "Käyttäjänimessä täytyy olla kirjaimia"
+            return render_template("register.html", error=error)
+        if password1 != password2:
+            error = 'Salasanat eivät täsmää'
+            return render_template("register.html", error=error)
+        length = len(password1)
+        if length < 5:
+            error = "Salasanan tulee olla vähintään 5 merkkiä pitkä"
+            return render_template("register.html", error=error)
+        noletters = True
+        nocapital = True
+        nonumber = True
+        for i in password1:
+            if noletters or nocapital:
                 if i.isalpha():
                     noletters = False
-                    break
-            if noletters:
-                error = "Käyttäjänimessä täytyy olla kirjaimia"
-                return render_template("register.html", error=error)
-            if password1 != password2:
-                error = 'Salasanat eivät täsmää'
-                return render_template("register.html", error=error)
-            length = len(password1)
-            if length < 5:
-                error = "Salasanan tulee olla vähintään 5 merkkiä pitkä"
-                return render_template("register.html", error=error)
-            noletters = True
-            nocapital = True
-            nonumber = True
-            for i in password1:
-                if noletters or nocapital:
-                    if i.isalpha():
-                        noletters = False
-                    if i in string.ascii_uppercase:
-                        nocapital = False
-                if nonumber:
-                    if str(i) in "0123456789":
-                        nonumber = False
-            if noletters:
-                error = "Salasanassa tulee olla vähintään 1 kirjain"
-                return render_template("register.html", error=error)
-            if nocapital:
-                error = "Salasanassa tulee olla vähintään 1 isokirjain"
-                return render_template("register.html", error=error)
+                if i in string.ascii_uppercase:
+                    nocapital = False
             if nonumber:
-                error = "Salasanassa tulee olla vähintään 1 numero"
-                return render_template("register.html", error=error)
-            hash_value = generate_password_hash(password1)
-            sql = "INSERT INTO users (username, password, admin) VALUES (:username, :password, '0')"
-            db.session.execute(sql, {"username":username, "password":hash_value})
-            db.session.commit()
-            flash("Käyttäjä luotu onnistuneesti")
-            return redirect("/login")
+                if str(i) in "0123456789":
+                    nonumber = False
+        if noletters:
+            error = "Salasanassa tulee olla vähintään 1 kirjain"
+            return render_template("register.html", error=error)
+        if nocapital:
+            error = "Salasanassa tulee olla vähintään 1 isokirjain"
+            return render_template("register.html", error=error)
+        if nonumber:
+            error = "Salasanassa tulee olla vähintään 1 numero"
+            return render_template("register.html", error=error)
+        hash_value = generate_password_hash(password1)
+        sql = "INSERT INTO users (username, password, admin) VALUES (:username, :password, '0')"
+        db.session.execute(sql, {"username":username, "password":hash_value})
+        db.session.commit()
+        flash("Käyttäjä luotu onnistuneesti")
+        return redirect("/login")
 
 @app.route("/logout")
 def logout():
     del session["username"]
     flash("Kirjauduttu ulos onnistuneesti")
     return redirect("/")
+
+def is_user():
+    user = False
+    try:
+        if session["username"]:
+            user = True
+    except:
+        pass
+    return user
+
+def is_admin():
+    admin = False
+    if is_user():
+        sql = "SELECT admin FROM users WHERE username=:username"
+        username = session["username"]
+        result = db.session.execute(sql, {"username":username})
+        a = result.fetchone()[0]
+        if a == '1':
+            admin = True
+    return admin
