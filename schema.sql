2284775347b63c8310077ec5a6f4dbbc373e23c5
Tuomas Marenk <tuomas.marenk@gmail.com>
2022-04-02 10:38:34 +0300

    Fixed typo

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE movies (id SERIAL PRIMARY KEY, name TEXT);
 CREATE TABLE information (id SERIAL PRIMARY KEY, movie_id INTEGER REFERENCES movies, year INTEGER, length INTEGER, genre TEXT);
-CREATE TABLE reviews (id SERIAL PRIMARY KEY, movie_id INTEGER REFERENCES movies, stars INTEGER, review TEXT, left_at TIMESTAMP, user_id INTEGER REFERENCEES users);
+CREATE TABLE reviews (id SERIAL PRIMARY KEY, movie_id INTEGER REFERENCES movies, stars INTEGER, review TEXT, left_at TIMESTAMP, user_id INTEGER REFERENCES users);
 CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT, admin BOOLEAN);
 CREATE TABLE description (id SERIAL PRIMARY KEY, movie_id INTEGER REFERENCES movies, description TEXT);
